6ecf22aa6666270bfee7a49d69ab83f5d8f124f0
han97901 <han97901@gmail.com>
2025-10-27 00:53:12 +0900

    fix(view): 최종 우승자 출력 폼 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,34 +1,18 @@
 package racingcar.view;
 
 import java.util.List;
-import racingcar.domain.Car;
 import racingcar.domain.Cars;
 
 public class OutputView {
-    /*
-    FIXME: 실제 자동차 이름을 출력하도록 변경
-     */
+
     public void printRacing(Cars cars){
-        System.out.println("실행 결과");
-        for (Car car : cars.getCars()){
-            System.out.print(car.getName().getValue() + " : ");
-            int count = car.getPosition();
-            while(count-- > 0){
-                System.out.print("-");
-            }
-            System.out.println();
-        }
+        System.out.println();
+        cars.forEach(car -> {
+            System.out.println(car.getNameValue() + " : " + car.getPositionDisplay());
+        });
     }
-    /*
-   FIXME: 실제 우승자 이름을 출력하도록 변경
-    */
+
     public void printWinner(List<String> winners){
-        System.out.print("최종 우승자");
-        for (String winner : winners){
-            System.out.print(winner);
-        }
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
-
-
-
 }
